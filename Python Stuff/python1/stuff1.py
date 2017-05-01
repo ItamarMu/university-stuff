dd9173824ce938a9ad6fcb730eb5181976336ac7
ItamarMu <freg10@gmail.com>
2017-05-01 16:41:11 +0300

    st

--- Python Stuff/python1/stuff1.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import re
-
-data = urllib.request.urlopen("https://www.google.com/").read()
-wanted = re.findall(r'<title>(.*)</title>',str(data),re.I|re.M)
-print(wanted)
-input()
